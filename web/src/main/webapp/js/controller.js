54ab4538c5019addc8f029e6c54d42dd8234fa3b
andreluiznsilva <andreluiznsilva@gmail.com>
2013-09-04 20:39:59 -0300

    exercicio 1
    Signed-off-by: andreluiznsilva <andreluiznsilva@gmail.com>

--- web/src/main/webapp/js/controller.js
+++ web/src/main/webapp/js/controller.js
@@ -1,6 +1,4 @@
-function Controller($scope, $rootScope, $window, $filter) {
-
-	$scope.openned = false;
+function Controller($scope) {
 
 	$scope.employees = [ {
 		"id" : 1,
@@ -24,50 +22,6 @@ function Controller($scope, $rootScope, $window, $filter) {
 
 	$scope.functions = [ "MANAGER", "PO", "DEVELOPER" ];
 
-	$scope.edit = function(employee) {
-		$scope.openForm(angular.copy(employee));
-	}
-
-	$scope.create = function() {
-		$scope.openForm({});
-	}
-
-	$scope.cancel = function() {
-		$scope.closeForm();
-	}
-
-	$scope.save = function() {
-
-		$scope.employee.birthday = Utils.parseDate($scope.formatedBirthday);
-
-		if ($scope.employee.id) {
-			Utils.replaceItemById($scope.employee, $scope.employees);
-		} else {
-			Utils.addItem(result, $scope.employees);
-		}
-
-		$scope.closeForm();
-
-	}
-
-	$scope.remove = function() {
-		if ($scope.employee && $scope.employee.id) {
-			Utils.removeItemById($scope.employee, $scope.employees);
-		}
-		$scope.closeForm();
-	}
-
-	$scope.openForm = function(employee) {
-		$scope.employee = employee;
-		// $scope.formatedBirthday = $filter('date')($scope.employee.birthday,
-		// 'dd/MM/yyyy');
-		$scope.formatedBirthday = Utils.formatDate($scope.employee.birthday);
-		$scope.openned = true;
-	}
-
-	$scope.closeForm = function() {
-		$scope.employee = {};
-		$scope.openned = false;
-	}
+	// Utilizar funções do Utils para manipular os arrays e formatar datas
 
 }
