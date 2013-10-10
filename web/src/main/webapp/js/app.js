7fbc318d7f513ea24244c8a6409c749e53003496
andre.nascimento <andre.nascimento@dextra-sw.com>
2013-10-10 08:38:07 -0300

    diretivas e filtros

--- web/src/main/webapp/js/app.js
+++ web/src/main/webapp/js/app.js
@@ -1,13 +1,23 @@
 var appModule = angular.module('app', []);
 
 appModule.filter("reverse", function() {
-
+	return function(input) {
+		return input;
+	};
 });
 
 appModule.directive("tooltip", function() {
-
+	return {
+		restrict : 'A',
+		link : function(scope, element, attributes) {
+		}
+	}
 });
 
 appModule.directive("alert", function() {
-
+	return {
+		restrict : 'E',
+		link : function(scope, element, attributes) {
+		}
+	}
 });
