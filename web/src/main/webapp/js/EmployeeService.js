1119895572563c0bfaf796746bb73337d211292f
andre.nascimento <andre.nascimento@dextra-sw.com>
2013-10-10 10:58:44 -0300

    ajsutes

---
+++ web/src/main/webapp/js/EmployeeService.js
@@ -0,0 +1,31 @@
+function EmployeeService($http, $log) {
+	
+	var error = function(data, status, func, request) {
+		$log.error('Http Error ' + status + " : " + data);
+	};
+
+	this.list = function(callback) {
+		$http.get('/dextra-angular/api/employees').success(callback).error(error);
+	};
+	
+	this.listFunctions = function(callback) {
+		$http.get('/dextra-angular/api/functions').success(callback).error(error);
+	};
+
+	this.get = function(id, callback) {
+		$http.get('/dextra-angular/api/employees/' + id).success(callback).error(error);
+	};
+
+	this.update = function(employee, callback) {
+		$http.put('/dextra-angular/api/employees/' + employee.id, employee).success(callback).error(error);
+	};
+
+	this.create = function(employee, callback) {
+		$http.post('/dextra-angular/api/employees', employee).success(callback).error(error);
+	};
+
+	this.remove = function(employee, callback) {
+		$http.delete('/dextra-angular/api/employees/' + employee.id).success(callback).error(error);
+	};
+
+};
