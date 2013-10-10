d321b77c49c58a3e596b5fc1e1de91b95d86e39a
andre.nascimento <andre.nascimento@dextra-sw.com>
2013-10-10 16:57:39 -0300

    ajustes

--- web/src/main/webapp/test/spec/EmployeeSpec.js
+++ web/src/main/webapp/test/spec/EmployeeSpec.js
@@ -1,35 +1,64 @@
-describe('Employee Test', function() {
+describe('Employee Service Test', function() {
 
 	var page = new IndexPage();
 	var employee = {
 			name : 'test',
-			birthday : 123456,
+			birthday : '22/07/84',
 			salary : 1576,
 			function : 'DEVELOPER'
 		};
-
+	
 	beforeEach(function() {
 		browser().navigateTo('../reset');
 		browser().navigateTo('../');
 	});
-	
+
 	it('list employees', function() {
 		expect(page.countEmployees()).toBe(3);
 	});
-	
-	it('cancel employee creation', function() {
-	});
-	
-	it('cancel employee update', function() {
-	});
 
 	it('create a employee', function() {
+		employee = {
+			name : 'a',
+			birthday : '22/07/84',
+			salary : 3333,
+			function : 'MANAGER'
+		};
+		page.clickCreate();
+		page.fillForm(employee)
+		page.clickSave();
+		expect(page.countEmployees()).toBe(4);
 	});
-	
+
 	it('update a employee', function() {
+		employee = {
+			name : 'b',
+			birthday : '22/07/84',
+			salary : 45637,
+			function : 'PO'
+		};
+		page.clickEmployee(1);
+		page.fillForm(employee)
+		page.clickSave();
+		expect(page.countEmployees()).toBe(3);
 	});
-	
+
 	it('delete a employee', function() {
+		page.clickEmployee(2);
+		page.clickDelete();
+		expect(page.countEmployees()).toBe(2);
 	});
-	
+
+	it('cancel employee creation', function() {
+		page.clickEmployee(1);
+		page.fillForm(employee);
+		page.clickCancel();
+	});
+
+	it('cancel employee update', function() {
+		page.clickEmployee(0);
+		page.fillForm(employee);
+		page.clickCancel();
+	});
+
 });
