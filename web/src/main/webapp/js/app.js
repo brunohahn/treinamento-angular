d0e7b144f0b7bdec75da641a739551f5464fa0aa
andre.nascimento <andre.nascimento@dextra-sw.com>
2013-10-10 09:15:34 -0300

    exercicio preparado

--- web/src/main/webapp/js/app.js
+++ web/src/main/webapp/js/app.js
@@ -5,19 +5,34 @@ appModule.filter("reverse", function() {
 		return input;
 	};
 });
-
 appModule.directive("tooltip", function() {
 	return {
 		restrict : 'A',
 		link : function(scope, element, attributes) {
+			element.tooltip({
+				trigger : 'hover',
+				title : attributes.tooltip,
+				container : 'body'
+			});
 		}
 	}
 });
-
 appModule.directive("alert", function() {
 	return {
 		restrict : 'E',
-		link : function(scope, element, attributes) {
-		}
+		replace: true,
+	    transclude: true,
+		template: '<div class="alert"><a href="#" class="close" data-dismiss="alert">&times;</a><strong ng-transclude></strong></div>'
+	}
+});
+
+appModule.filter("abbreviate", function() {
+	return function(input) {
+		return input;
+	};
+});
+appModule.directive("icon", function() {
+	return {
+		restrict : 'E',
 	}
 });
